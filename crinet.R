4496e4e785fd8e1fe316e41e57a13435bf999a55
Ziynet Nesibe <32200164+ziynet@users.noreply.github.com>
2020-06-12 14:38:40 -0500

    Update crinet.R
    Ready to use

--- crinet.R
+++ crinet.R
@@ -7,17 +7,17 @@
 
 # Load functions
 source("crinet.functions.R")
-# load data
-load(paste(workingSpace, saveDir, paste("CRINET_data.rda", sep = ""), sep = "/"))
-load(paste(workingSpace, saveDir, paste("CRINET_interactions.rda", sep = ""), sep = "/"))
+# load all data or load sample data
+# load(paste(workingSpace, saveDir, paste("CRINET_data.rda", sep = ""), sep = "/")); load(paste(workingSpace, saveDir, paste("CRINET_interactions.rda", sep = ""), sep = "/"))
+load(paste(workingSpace, saveDir, paste("CRINET_sample.rda", sep = ""), sep = "/"))
 
-core.no = 68
-case.no = 1
+# Specify the core no to use in parallel processing
+core.no = 10
 
-# hyperparameters
-singleErExp.thr = 0; comMir.thr = 1; hgeo.thr = 0.05; ceRNApartcor.thr = 0.3; ceRNApartcorPvalue.thr = 0.05; sumErExp.thr = -0.01
+# Select hyperparameters
+singleErExp.thr = -0.01; comMir.thr = 1; hgeo.thr = 0.01; ceRNApartcor.thr = 0.55; ceRNApartcorPvalue.thr = 0.01; sumErExp.thr = -0.01
 
-# generate Effective Regulation
+# Generate Effective Regulation
 ERs = generate_ERs(RNA = RNA,
                    miRNA = miRNA,
                    interactions = miRNA.target.interactions,
@@ -30,47 +30,53 @@ candidate.pairs = generate_candidate_pairs(ERs)
 candidate.pairs$common.miRs = eliminate_wrt_commonRegulator(pairs = candidate.pairs,
                                                             interactions = miRNA.target.interactions,
                                                             core.no = core.no)
-candidate.pairs = candidate.pairs[candidate.pairs$common.miRs>=comMir.thr,]
+candidate.pairs = candidate.pairs[candidate.pairs$common.miRs >= comMir.thr,]
+
 # Eliminate candidate pairs with respect to hypergeometric test based on common number of miRNAs
 candidate.pairs$hypergeo = eliminate_wrt_hypergeo(pairs = candidate.pairs,
                                                   interactions = miRNA.target.interactions,
                                                   core.no = core.no)
-candidate.pairs = candidate.pairs[candidate.pairs$hypergeo<hgeo.thr,]
+candidate.pairs = candidate.pairs[candidate.pairs$hypergeo < hgeo.thr,]
+
 # Eliminate candidate pairs with respect to significant correlation among their RNA expression excluding copy number aberration effect
 candidate.pairs$ceRNA.part.cor = eliminate_wrt_partialCor(RNA = RNA,
                                                           cna = cna,
                                                           pairs = candidate.pairs,
                                                           core.no = core.no)
-candidate.pairs = candidate.pairs[candidate.pairs$ceRNA.part.cor>ceRNApartcor.thr,]
+candidate.pairs = candidate.pairs[candidate.pairs$ceRNA.part.cor > ceRNApartcor.thr,]
+
 candidate.pairs$ceRNA.part.corP = eliminate_wrt_partialCorP(RNA = RNA,
                                                             cna = cna,
                                                             pairs = candidate.pairs,
                                                             core.no = core.no)
-candidate.pairs = candidate.pairs[candidate.pairs$ceRNA.part.corP<ceRNApartcorPvalue.thr,]
+
+candidate.pairs = candidate.pairs[candidate.pairs$ceRNA.part.corP < ceRNApartcorPvalue.thr,]
+
 # Eliminate candidate pairs with respect to significant regulation collectively
 candidate.pairs$sumErExp = eliminate_wrt_collectiveRegulation(RNA = RNA,
                                                               ERs = ERs,
                                                               pairs = candidate.pairs,
                                                               core.no = core.no)
-candidate.pairs = candidate.pairs[candidate.pairs$sumErExp<sumErExp.thr,]
+candidate.pairs = candidate.pairs[candidate.pairs$sumErExp < sumErExp.thr,]
+
 # 1.bootstrapping
-candidate.pairs$ceRNA.part.cor.bootstrapP = eliminate_wrt_partialCor_bootstrapPvalue(RNA = RNA,
-                                                                                     cna = cna,
-                                                                                     pairs = candidate.pairs,
-                                                                                     thr = ceRNApartcor.thr,
-                                                                                     core.no = core.no)
-candidate.pairs = candidate.pairs[candidate.pairs$ceRNA.part.cor.bootstrapP<0.05,]
+candidate.pairs$ceRNA.part.cor.bootstrapP = eliminate_wrt_partialCor_bootstrapP(RNA = RNA,
+                                                                                cna = cna,
+                                                                                pairs = candidate.pairs,
+                                                                                core.no = core.no)
+candidate.pairs = candidate.pairs[candidate.pairs$ceRNA.part.cor.bootstrapP > ceRNApartcor.thr,]
+
 # 2.bootstrapping
-candidate.pairs$sumErExp.bootstrapP = eliminate_wrt_collectiveRegulation_bootstrapPvalue(RNA = RNA,
-                                                                                         ERs = ERs,
-                                                                                         pairs = candidate.pairs,
-                                                                                         thr = sumErExp.thr,
-                                                                                         core.no = core.no)
+candidate.pairs$sumErExp.bootstrapP = eliminate_wrt_collectiveRegulation_bootstrapP(RNA = RNA,
+                                                                                    ERs = ERs,
+                                                                                    pairs = candidate.pairs,
+                                                                                    core.no = core.no)
 
-candidate.pairs = candidate.pairs[candidate.pairs$sumErExp.bootstrapP<0.05,]
+candidate.pairs = candidate.pairs[candidate.pairs$sumErExp.bootstrapP < sumErExp.thr,]
+
 # Network Deconvolution
-df = generate_deconv_input(pairs = candidate.pairs, scores = candidate.pairs$ceRNA.part.cor, needNormalization = T, deconv_in = paste0(def,"Deconv_input.mat"))
+df = generate_deconv_input(pairs = candidate.pairs, scores = candidate.pairs$ceRNA.part.cor, needNormalization = T, deconv_in = paste0("Deconv_input.mat"))
 
-# Run network deconvolution using Matlab [generated input is named as: "Deconv_input.mat". Output should be put into workingSpace named as "Deconv_output.mat"]
-
-inferred.pairs = eliminate_wrt_networkDeconvolution(pairs = candidate.pairs, deconv_out = paste0(def,"Deconv_output.mat"), df = df)
+# Run network deconvolution algorithm (ND.m) using Matlab 
+# [Input for Matlab is generated and named as: "Deconv_input.mat". Output of Matlab should be put into workingSpace named as "Deconv_output.mat"]
+inferred.pairs = eliminate_wrt_networkDeconvolutionWithPercentage(pairs = candidate.pairs, deconv_out = paste0("Deconv_output.mat"), df = df, perc = .33)
